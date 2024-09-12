3540250a7a609fbac51cbdc979698a1283ff4d7f
Egor Olenin <egorolenin2005@mail.ru>
2024-09-12 19:17:46 +0300

    Code style fix

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,10 +3,8 @@ package ru.yandex.practicum.filmorate.controller;
 import org.slf4j.Logger;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
