d43cb823b46b3c88d5ff55108fb33d0b426cfa99
Egor Olenin <egorolenin2005@mail.ru>
2024-09-30 00:31:36 +0300

    Code style fix

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +10,7 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-@Getter @Setter
+@Data
 public class User {
     private Long id;
     @NotBlank(message = "Почта не должна быть пустой")
