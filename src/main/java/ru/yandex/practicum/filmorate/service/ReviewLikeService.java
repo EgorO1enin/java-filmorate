75494000b05918f165345f853e3021e1156936e4
Aleksandr.Abramovich <hause.abramovi44@gmail.com>
2025-02-04 22:33:49 +0300

    - task "Функциональность «Лента событий». 3 SP" - add /users/{id}/feed

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewLikeService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewLikeService.java
@@ -2,26 +2,34 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.EventType;
+import ru.yandex.practicum.filmorate.model.OperationEvent;
 import ru.yandex.practicum.filmorate.storage.ReviewLikeStorage;
+import ru.yandex.practicum.filmorate.storage.impl.UserDbStorageImpl;
 
 @RequiredArgsConstructor
 @Service
 public class ReviewLikeService {
     private final ReviewLikeStorage reviewLikeStorage;
+    private final UserDbStorageImpl userDbStorage;
 
     public void addLike(Long reviewId, Long userId) {
         reviewLikeStorage.addLike(reviewId, userId);
+        userDbStorage.addFeed(userId, EventType.LIKE, OperationEvent.ADD, reviewId);
     }
 
     public void addDislike(Long reviewId, Long userId) {
         reviewLikeStorage.addDislike(reviewId, userId);
+        userDbStorage.addFeed(userId, EventType.LIKE, OperationEvent.ADD, reviewId);
     }
 
     public void removeLike(Long reviewId, Long userId) {
         reviewLikeStorage.removeLike(reviewId, userId);
+        userDbStorage.addFeed(userId, EventType.LIKE, OperationEvent.REMOVE, reviewId);
     }
 
     public void removeDislike(Long reviewId, Long userId) {
         reviewLikeStorage.removeDislike(reviewId, userId);
+        userDbStorage.addFeed(userId, EventType.LIKE, OperationEvent.REMOVE, reviewId);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -2,8 +2,11 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.EventType;
+import ru.yandex.practicum.filmorate.model.OperationEvent;
 import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.storage.ReviewStorage;
+import ru.yandex.practicum.filmorate.storage.impl.UserDbStorageImpl;
 
 import java.util.List;
 
@@ -11,6 +14,7 @@ import java.util.List;
 @Service
 public class ReviewService {
     private final ReviewStorage reviewStorage;
+    private final UserDbStorageImpl userDbStorage;
 
     public List<Review> getReviews(Long filmId, int count) {
         return reviewStorage.getReviews(filmId, count);
@@ -21,14 +25,18 @@ public class ReviewService {
     }
 
     public Review addReview(Review review) {
+        userDbStorage.addFeed(review.getUserId(), EventType.REVIEW, OperationEvent.ADD, review.getReviewId());
         return reviewStorage.addReview(review);
     }
 
     public Review updateReview(Review review) {
+        userDbStorage.addFeed(review.getUserId(), EventType.REVIEW, OperationEvent.UPDATE, review.getReviewId());
         return reviewStorage.updateReview(review);
     }
 
     public void removeReview(Long reviewId) {
+        Review review = reviewStorage.getReviewById(reviewId);
+        userDbStorage.addFeed(review.getUserId(), EventType.REVIEW, OperationEvent.REMOVE, review.getReviewId());
         reviewStorage.removeReview(reviewId);
     }
 }
