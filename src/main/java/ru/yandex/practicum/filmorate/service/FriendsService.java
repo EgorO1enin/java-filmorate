bab5275fc6b6a9d0a538a2fa7060012188302b05
Aleksandr.Abramovich <hause.abramovi44@gmail.com>
2025-02-04 23:07:21 +0300

    - task "Функциональность «Лента событий». 3 SP" - add /users/{id}/feed - v4

--- src/main/java/ru/yandex/practicum/filmorate/service/FriendsService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FriendsService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.EventType;
 import ru.yandex.practicum.filmorate.model.OperationEvent;
-import ru.yandex.practicum.filmorate.storage.impl.FriendsDbStorageImpl;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.impl.FriendsDbStorageImpl;
 import ru.yandex.practicum.filmorate.storage.impl.UserDbStorageImpl;
